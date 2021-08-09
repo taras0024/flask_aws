e3397e3cf0017d2ecb6c9683ea6642bf0cad02cb
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-08-09 19:04:06 +0300

    Hello AWS

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    hello_world = 'Hello World!!!'
+    hello_world = 'Hello AWS'
     return render_template('index.html', temp=hello_world)
 
 
