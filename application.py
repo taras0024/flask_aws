c9a0f0fcffd6eb5bdb8de3db9fd947dfcd129db7
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-08-10 14:16:58 +0300

    render template

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -6,9 +7,9 @@ def say_hello(username = "World"):
 
 # some bits of text for the page.
 header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+    <html>\n<head> <title>TASK Hello world</title> </head>\n<body>'''
 instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    <p><em>Hint</em>:Append a username
     to the URL (for example: <code>/Thelonious</code>) to say hello to
     someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
@@ -18,8 +19,12 @@ footer_text = '</body>\n</html>'
 application = Flask(__name__)
 
 # add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+# application.add_url_rule('/', 'index', (lambda: header_text +
+#     say_hello() + instructions + footer_text))
+
+@application.route('/')
+def hello_world():
+    return render_template('index.html')
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
