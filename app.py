526e504eaefee241289dffc9565d987983226b10
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-08-10 12:56:44 +0300

    edit template and func

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    hello_world = 'Hello AWS'
-    return render_template('index.html', temp=hello_world)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
